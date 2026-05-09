4bd2624ff66193ca0b427cac300f11360f86941c
aemirik <aemirik@example.com>
2026-05-09 10:29:54 +0300

    Fix PHP built-in server static assets

--- public/index.php
+++ public/index.php
@@ -5,6 +5,14 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
+if (php_sapi_name() === 'cli-server') {
+    $path = parse_url($_SERVER['REQUEST_URI'] ?? '/', PHP_URL_PATH) ?: '/';
+
+    if ($path !== '/' && is_file(__DIR__.$path)) {
+        return false;
+    }
+}
+
 // Determine if the application is in maintenance mode...
 if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;
